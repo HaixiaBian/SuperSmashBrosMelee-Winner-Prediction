a0d13f9ce4938eaea48dc67507268f37b7052334
Oliver Spohngellert <spohngellert.o@husky.neu.edu>
2020-03-10 12:26:18 -0400

    Adding documentation.

--- slp-to-json.js
+++ slp-to-json.js
@@ -5,9 +5,8 @@ const out_folder = "./out/"
 const path = require('path');
 
 
+// Gets every file within the folder defined above containing the .slp files
 fs.readdirSync(slip_folder).forEach(file => {
-
-
 
 	const game = new SlippiGame(path.join(slip_folder, file));
 
@@ -19,6 +18,10 @@ fs.readdirSync(slip_folder).forEach(file => {
 	const metadata = game.getMetadata();
 	console.log(metadata['players']);
 
+	/**
+	 Gets the index of which player is using jigglypuff, which character is using fox.
+	 This is important as there are 4 character slots, and players can be on any of the 4.
+	**/
 	var jig = 0;
 	var other = 0;
 	if(settings['players'][0]['characterId'] == 15) {
@@ -31,33 +34,32 @@ fs.readdirSync(slip_folder).forEach(file => {
 	}
 
 	console.log(jig)
-	// Get computed stats - openings / kill, conversions, etc
-	// const stats = game.getStats();
-	// console.log(stats);
 
 	// Get frames – animation state, inputs, etc
 	// This is used to compute your own stats or get more frame-specific info (advanced)
 	const frames = game.getFrames();
 
-	// console.log(frames[0]['players']); // Print frame when timer starts counting down
-
 	const last = metadata['lastFrame']
 	console.log(last)
-
+	// Finding out whether jigglypuff won
 	const puff_won = frames[last]['players'][jig]['post']['stocksRemaining'] > 0
 
 	console.log(puff_won)
 
-	var f2 = {}
+	// Recreating the frames dict using only ever nth frame
+	var frames2 = {}
 
 	for(i=1;i<last+1;i++) {
 		if(i%60==0) {
-			f2[i] = frames[i]
+			frames2[i] = frames[i]
 		}
 	}
+	// Saving the information we got in settings, which will be written to the JSON file
 	settings['winner'] = puff_won
 	settings['jig_id'] = jig
 	settings['other_id'] = other
+
+	// Get the file name, then create the folder where the json files will be
 	const file_name = file.split(".")[0]
 	
 	if (!fs.existsSync(path.join(out_folder, file_name))){
@@ -65,7 +67,8 @@ fs.readdirSync(slip_folder).forEach(file => {
 	}
 
 
-	fs.writeFileSync(path.join(out_folder, file_name, "game.json"), JSON.stringify(f2), function(err) {
+	// Write the JSON files
+	fs.writeFileSync(path.join(out_folder, file_name, "game.json"), JSON.stringify(frames2), function(err) {
 		if(err) {
 			return console.log("err")
 		}
