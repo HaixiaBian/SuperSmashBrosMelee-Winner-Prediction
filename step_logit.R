4817c90725331cf33cbc701c68ea7ef09d6ab625
Weichen Li <liweichen6@gmail.com>
2020-03-28 17:39:09 -0400

    update to utilize train/test split; add prediction and ROC

--- step_logit.R
+++ step_logit.R
@@ -1,10 +1,20 @@
+library(ROCR)
 # library(speedglm)
 # library(parglm)
 
+
+
 # Data
-melee = read.csv("all_data.csv")
-melee_clean = within(melee, rm(X, frame))
-melee_clean = na.omit(melee_clean)
+# melee = read.csv("all_data.csv")
+# melee_clean = within(melee, rm(frame, game_id, tourn_id))
+# melee_clean = na.omit(melee_clean)
+train = read.csv("train.csv")
+train = within(train, rm(frame, game_id, tourn_id))
+train = na.omit(train)
+
+test = read.csv("test.csv")
+test = within(test, rm(frame, game_id, tourn_id))
+test = na.omit(test)
 
 
 
@@ -18,7 +28,9 @@ melee_clean = na.omit(melee_clean)
 # logit_interact = speedglm(formula = winner ~ . + . ^ 2,
 #                           data = melee_clean,
 #                           family = binomial())
-#
+
+
+
 # # Parglm
 # logit_null = parglm(formula = winner ~ 1,
 #                     data = melee_clean,
@@ -30,15 +42,17 @@ melee_clean = na.omit(melee_clean)
 #                         data = melee_clean,
 #                         family = binomial)
 
+
+
 # Glm
 logit_null = glm(formula = winner ~ 1,
-                 data = melee_clean,
+                 data = train,
                  family = binomial)
 logit_full = glm(formula = winner ~ .,
-                 data = melee_clean,
+                 data = train,
                  family = binomial)
 logit_interact = glm(formula = winner ~ . + . ^ 2,
-                     data = melee_clean,
+                     data = train,
                      family = binomial)
 
 
@@ -48,14 +62,16 @@ step_full = step(
   object = logit_null,
   scope = formula(logit_full),
   direction = "both",
-  k = log(nrow(melee_clean))
+  k = log(nrow(train))
 )
 step_interact = step(
   object = step_full,
   scope = formula(logit_interact),
   direction = "both",
-  k = log(nrow(melee_clean))
+  k = log(nrow(train))
 )
+
+
 
 # BIC
 models = list(
@@ -65,10 +81,42 @@ models = list(
   StepFull = step_full,
   StepInteract = step_interact
 )
-bic = sapply(models, extractAIC, k = log(nrow(melee_clean)))[2,]
+bic = sapply(models, extractAIC, k = log(nrow(train)))[2, ]
 ebic = exp(-.5 * (bic - min(bic)))
 round(ebic / sum(ebic), 8)
+
+
 
 # Summary
 summary(step_interact)
 summary(logit_interact)
+
+
+# Prediction
+threshold = 0.5
+
+# Train data
+predictTrain = predict(step_interact, type = "response")
+ROCRpred = prediction(predictTrain, train$winner)
+ROCRperf = performance(ROCRpred, "tpr", "fpr")
+plot(
+  ROCRperf,
+  colorize = TRUE,
+  print.cutoffs.at = threshold,
+  text.adj = c(-0.5, 0.5)
+)
+table(train$winner, predictTrain >= threshold)
+(136048 + 126532)/365572
+
+# Test data
+predictTest = predict(step_interact, type = "response", newdata = test)
+ROCRpred = prediction(predictTest, test$winner)
+ROCRperf = performance(ROCRpred, "tpr", "fpr")
+plot(
+  ROCRperf,
+  colorize = TRUE,
+  print.cutoffs.at = threshold,
+  text.adj = c(-0.5, 0.5)
+)
+table(test$winner, predictTest >= threshold)
+(39847 + 37265)/104048
